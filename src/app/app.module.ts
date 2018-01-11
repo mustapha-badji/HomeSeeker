ddec27269cbab7612cb1e0942c5d7e90f254af7f
mustapha.badji <mustapha.badji@live.com>
2018-01-11 23:50:45 +0100

    Implémentation du routage des components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
@@ -7,6 +8,12 @@ import { AjouterAppartementComponent } from './ajouter-appartement/ajouter-appar
 import { ListeAppartementComponent } from './liste-appartement/liste-appartement.component';
 import { AccueilComponent } from './accueil/accueil.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+
+const appRoutes: Routes = [
+  { path: 'Accueil', component: AccueilComponent },
+  { path: 'Appartement', component: AjouterAppartementComponent },
+  { path: 'Appartements', component: ListeAppartementComponent}
+];  
 
 
 @NgModule({
@@ -18,6 +25,7 @@ import { NavigationBarComponent } from './navigation-bar/navigation-bar.componen
     NavigationBarComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes,{ enableTracing: true }), // <-- debugging purposes only
     BrowserModule
   ],
   providers: [],
